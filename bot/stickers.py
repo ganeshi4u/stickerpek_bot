55a9ab44210a8f99967aaf9a557dc28c18723896
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-05-07 23:08:17 +0530

    bg transparency using opencv and contours

--- bot/stickers.py
+++ bot/stickers.py
@@ -1,6 +1,8 @@
 import logging
 import os
 import math
+import cv2
+import numpy as np
 from PIL import Image
 
 from telegram import Sticker
@@ -87,37 +89,82 @@ class StickerFile:
     def prepare_png(self, update, subdir=''):
         logger.info('preparing png (source file: %s)', self._downloaded_file_path)
 
-        im = Image.open(self._downloaded_file_path)
+        im = cv2.imread(self._downloaded_file_path)
         transparency_state = Database(config.sqlite.filename).get_transparency_state(update.effective_user.id)
+
+        # transparency parameters
+        BLUR = 21
+        CANNY_TRESH_1 = 10
+        CANNY_TRESH_2 = 200
+        MASK_DILATE_ITER = 25
+        MASK_ERODE_ITER = 25
+        MASK_COLOR = (0.0,0.0,1.0)
 
         logger.debug('original image size: %s', im.size)
         self._size_original = im.size
-        if (im.size[0] > 512 or im.size[1] > 512) or (im.size[0] != 512 and im.size[1] != 512):
+        if (np.size(im, 0) > 512 or np.size(im, 1) > 512) or (np.size(im, 0) != 512 and np.size(im, 1) != 512):
             logger.debug('resizing file because one of the sides is > 512px or at least one side is not 512px')
-            correct_size = get_correct_size(im.size)
+            correct_size = get_correct_size(np.size(im))
             self._size_resized = correct_size
-            im = im.resize(correct_size, Image.ANTIALIAS)
+            im = cv2.resize(im, dsize=(correct_size[0], correct_size[1]))
         else:
             logger.debug('original size is ok')
 
         if 'on' in transparency_state:
-            im = im.convert("RGBA")
-            datas = im.getdata()
-            newData = []
+            # Read image
+            #im = cv2.imdecode(im)
+            gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
-            for item in datas:
-                if item[0] == 255 and item[1] == 255 and item[2] == 255:
-                    newData.append((255, 255, 255, 0))
-                else:
-                    newData.append(item)
-            im.putdata(newData)
+            # Edge detection
+            edges = cv2.Canny(gray, CANNY_TRESH_1, CANNY_TRESH_2)
+            edges = cv2.dilate(edges, None)
+            edges = cv2.erode(edges, None)
+
+            # contours in edges, sort by area
+            contour_info = []
+            contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+            # Previously, for a previous version of cv2, this line was: 
+            #  contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+            # Thanks to notes from commenters, I've updated the code but left this note
+            for c in contours:
+                contour_info.append((
+                    c,
+                    cv2.isContourConvex(c),
+                    cv2.contourArea(c),
+                ))
+            contour_info = sorted(contour_info, key=lambda c: c[2], reverse=True)
+            max_contour = contour_info[0]
+
+            #-- Create empty mask, draw filled polygon on it corresponding to largest contour ----
+            # Mask is black, polygon is white
+            mask = np.zeros(edges.shape)
+            cv2.fillConvexPoly(mask, max_contour[0], (255))
+
+            #-- Smooth mask, then blur it --------------------------------------------------------
+            mask = cv2.dilate(mask, None, iterations=MASK_DILATE_ITER)
+            mask = cv2.erode(mask, None, iterations=MASK_ERODE_ITER)
+            mask = cv2.GaussianBlur(mask, (BLUR, BLUR), 0)
+            mask_stack = np.dstack([mask]*3)    # Create 3-channel alpha mask
+
+            #-- Blend masked img into MASK_COLOR background --------------------------------------
+            mask_stack  = mask_stack.astype('float32') / 255.0          # Use float matrices, 
+            im         = im.astype('float32') / 255.0                 #  for easy blending
+
+            masked = (mask_stack * im) + ((1-mask_stack) * MASK_COLOR) # Blend
+            masked = (masked * 255).astype('uint8')                     # Convert back to 8-bit
+
+            # split image into channels
+            c_red, c_green, c_blue = cv2.split(im)
+
+            # merge with mask got on one of a previous steps
+            im_a = cv2.merge((c_red, c_green, c_blue, mask.astype('float32') / 255.0))
+            
             self._png_path = 'tmp/{}transparency_converted_{}.png'.format(subdir, self._file.file_id)
         else:
             self._png_path = 'tmp/{}converted_{}.png'.format(subdir, self._file.file_id)
 
         logger.debug('saving PIL image object as png (%s)', self._png_path)
-        im.save(self._png_path, 'png')
-        im.close()
+        cv2.imwrite(self._png_path, im_a*255)
 
         return self._png_path
 
